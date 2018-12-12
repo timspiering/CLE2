d786fe7b64841633722b82048dc782dd394c28e8
Tim <timspiering@hotmail.com>
2018-12-12 11:50:56 +0100

    File structure setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Reservering</title>
+
+    {{-- CSS --}}
+    <link href="css/app.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+    <h1>Reservering</h1>
+    <input type='text' class="datepicker-here" data-position="right top" />
+</body>
+</html>
