5444f530f4670a67cbeff8c218df42270fbd27ae
Tim <timspiering@hotmail.com>
2018-12-12 11:51:23 +0100

    Add jquery & air-datepicker

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,13 @@
     <title>Reservering</title>
 
     {{-- CSS --}}
+    <link href="thirdparty/air-datepicker/css/datepicker.min.css" rel="stylesheet" type="text/css">
     <link href="css/app.css" rel="stylesheet" type="text/css">
 
+    {{-- JS --}}
+    <script src="thirdparty/jquery/jquery-3.3.1.min.js"></script>
+    <script src="thirdparty/air-datepicker/js/datepicker.min.js"></script>
+    <script src="thirdparty/air-datepicker/js/i18n/datepicker.en.js"></script>
 </head>
 <body>
     <h1>Reservering</h1>
