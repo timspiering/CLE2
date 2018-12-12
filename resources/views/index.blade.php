598210426c358548fe6fc5a533bd2774eae6d280
Tim <timspiering@hotmail.com>
2018-12-12 12:15:36 +0100

    Add bootstrap to project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,23 @@
-<html>
-<head>
-    <title>Reservering</title>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    {{-- CSS --}}
-    <link href="thirdparty/air-datepicker/css/datepicker.min.css" rel="stylesheet" type="text/css">
-    <link href="css/app.css" rel="stylesheet" type="text/css">
+        {{-- CSS --}}
+        <link href="thirdparty/air-datepicker/css/datepicker.min.css" rel="stylesheet" type="text/css">
+        <link href="thirdparty/bootstrap-4.1.3/css/bootstrap.min.css" type="text/css">
+        <link href="css/app.css" rel="stylesheet" type="text/css">
 
-    {{-- JS --}}
-    <script src="thirdparty/jquery/jquery-3.3.1.min.js"></script>
-    <script src="thirdparty/air-datepicker/js/datepicker.min.js"></script>
-    <script src="thirdparty/air-datepicker/js/i18n/datepicker.en.js"></script>
-    <script src="thirdparty/air-datepicker/js/i18n/datepicker.nl.js"></script>
-</head>
-<body>
-    <h1>Reservering</h1>
-    <input type='text' class="datepicker-here" data-position="right top" data-language='en' />
-</body>
+        {{-- JS --}}
+        <script src="thirdparty/jquery/jquery-3.3.1.min.js"></script>
+        <script src="thirdparty/air-datepicker/js/datepicker.min.js"></script>
+        <script src="thirdparty/air-datepicker/js/i18n/datepicker.en.js"></script>
+        <script src="thirdparty/air-datepicker/js/i18n/datepicker.nl.js"></script>
+        <script src="thirdparty/bootstrap-4.1.3/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <h1>Reservering</h1>
+        <input type='text' class="datepicker-here" data-position="right top" data-language='nl' />
+    </body>
 </html>
